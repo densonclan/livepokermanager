93efb620c094717c2929ade15984667a572eb235
Barry Denson <barry.denson@gaskandhawley.com>
2013-02-12 16:39:26 +0000

    Created a rails application and added players

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livepokermanager::Application.config.secret_token = '525d59a33903bd57e86e2c6215f6b8c7617be2768d6f58745cf3cf3e31856741c97da5614a82c12e1967fada25a155fea71f216dd5868ae32e084eccff4e41ca'
